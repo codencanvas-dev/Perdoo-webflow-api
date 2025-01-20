5a8053558f0ea5a8d808bc35ad94edb1451e1f79
Suhel <suhel7822@gmail.com>
2025-01-20 15:11:05 +0530

    collections fetched successfully

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { getDataFromCache } from './utils/db';
+import { Env } from './utils/types';
+import { getAllCollections, getWebflowClient } from './utils/webflow';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// instantiating webflow client & KV storage
+		const client = getWebflowClient(env as Env);
+		const KV = (env as Env).perdooCache;
+
+		// check if data is cached already and if it exists return that data from the cache itself
+		const cacheKey = `blogs_${(env as Env).PERDOO_SITE_ID}`;
+		const cachedBlogs = await getDataFromCache(cacheKey, KV);
+
+		const headerOptions = {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, OPTIONS',
+		};
+		if (cachedBlogs) return new Response(JSON.stringify(cachedBlogs), { headers: headerOptions });
+
+		//getting all collections
+		const { collections } = await getAllCollections(client, (env as Env).PERDOO_SITE_ID);
+
+		return new Response('Working on it');
 	},
 } satisfies ExportedHandler<Env>;
---
+++ src/utils/webflow.ts
@@ -0,0 +1,36 @@
+import { WebflowClient } from 'webflow-api';
+import { CollectionItemList, CollectionList } from 'webflow-api/api';
+import { Env } from './types';
+
+/**
+ * Creates and returns a WebflowClient instance.
+ *
+ * @param {Env} env - Environment configuration object containing the Webflow access token.
+ * @returns An instance of WebflowClient configured with the provided access token.
+ */
+export const getWebflowClient = (env: Env): WebflowClient => {
+	return new WebflowClient({
+		accessToken: env.WEBFLOW_ACCESS_TOKEN,
+	});
+};
+
+/**
+ * Fetches all collections for a given site ID using the Webflow client.
+ *
+ * @param {WebflowClient} client - The WebflowClient instance for API interaction.
+ * @param {string} siteId - The ID of the site for which collections are fetched.
+ * @returns A promise resolving to an object containing an array of WebflowCollection.
+ */
+export const getAllCollections = async (client: WebflowClient, siteId: string): Promise<CollectionList> => {
+	return await client.collections.list(siteId);
+};
+
+/**
+ *
+ * @param {WebflowClient} client
+ * @param {string} collectionId
+ * @returns A promise resolving to an object containing an array of WebflowCollection data.
+ */
+export const getCollectionData = async (client: WebflowClient, collectionId: string): Promise<CollectionItemList> => {
+	return await client.collections.items.listItemsLive(collectionId);
+};
